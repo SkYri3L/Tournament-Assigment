26aec8855dc7c5ebc76fbb61bedbd17037946c60
SkYri3L <Skyrimplaynet@gmail.com>
2022-03-17 11:52:49 +0000

    updated main.py with snake

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import button
 import os
 import TeamUser_Call
 import Snake
+import random
 
 
 # load button images
@@ -69,6 +70,100 @@ def Solo_Team():
         pygame.draw.rect(disp, color, input_box, 2)
         pygame.display.flip()
 
+#snake game defs
+def Your_score(score):
+    global value
+    value = score_font.render(str(score), True, red)
+    disp.blit(value, [6, 0])
+
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(disp, black, [x[0], x[1], snake_block, snake_block])
+
+
+def Snake_game():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close == True:
+            disp.fill(white)
+            #Draws the outline
+            pygame.draw.rect(disp, black, (0, 0, 680, 480), 5)
+            message("You Lost! Press Q to Quit", black)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        disp.fill(white)
+        pygame.draw.rect(disp, red, [foodx, foody, snake_block, snake_block, ])
+        pygame.draw.rect(disp, black, (0, 0, 680, 480), 5)
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pickle.dump(Length_of_snake - 1, open('Snake_Score.pkl', 'wb'))
+    print("Score Saved")
+
 # Creates the message
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
@@ -88,7 +183,7 @@ def gamerun():
 
             # Snake.py Run
             print("Snake.py has been opened")  # Testing if .py has been opened
-
+            Snake_game()
             with open('Snake_score.pkl', 'rb') as snake_scr:
                 snake_score = pickle.load(snake_scr)
                 print("Snake Score is " + str(snake_score))
